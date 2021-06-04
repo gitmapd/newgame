11465dd52b118f8e6c5eaeb890ed644e4b7eed9d
gitmapd <50059062+gitmapd@users.noreply.github.com>
2021-06-04 18:08:37 +0100

    Add files via upload

--- onegame.py
+++ onegame.py
@@ -4,7 +4,8 @@ screen_height=600
 pygame.init()
 screen=pygame.display.set_mode((screen_width,screen_height))
 running = True
-
+x=250
+y=250
 while running:
     for event in pygame.event.get():
 
@@ -15,7 +16,9 @@ while running:
 
     screen.fill((255, 255, 255))
 
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
+    pygame.draw.circle(screen, (0, 0, 255), (x,y), 75)
+    while x<250:
+        x+=0.003
+    y-=0.02
     pygame.display.flip()
 pygame.quit()
