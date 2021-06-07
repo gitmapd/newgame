04380d59b4acadf90cb0cca31832472aa714e5e9
gitmapd <50059062+gitmapd@users.noreply.github.com>
2021-06-07 21:48:54 +0100

    Add files via upload

--- onegame.py
+++ onegame.py
@@ -1,11 +1,13 @@
 import pygame
+from math import sin,cos,pi
 screen_width=800
 screen_height=600
 pygame.init()
 screen=pygame.display.set_mode((screen_width,screen_height))
 running = True
-x=250
-y=250
+x=200
+y=100
+
 while running:
     for event in pygame.event.get():
 
@@ -15,10 +17,11 @@ while running:
 
 
     screen.fill((255, 255, 255))
-
-    pygame.draw.circle(screen, (0, 0, 255), (x,y), 75)
-    while x<250:
-        x+=0.003
-    y-=0.02
+    for i in range(int(2*pi)):
+        x+=cos(i)
+        y+=sin(i)
+        pygame.draw.circle(screen, (0, 0, 255), (x,y), 75)
+        x+=0.00001
+        y+=0.00001
     pygame.display.flip()
 pygame.quit()
